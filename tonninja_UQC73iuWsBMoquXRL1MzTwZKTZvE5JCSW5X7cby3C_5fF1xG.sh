d3e9e2556b76e7e5c8733a21b2e5b89be4361dd8
xperiakamenev <123416737+xperiakamenev@users.noreply.github.com>
2024-02-04 20:16:11 +0200

    Add files via upload

---
+++ tonninja_UQC73iuWsBMoquXRL1MzTwZKTZvE5JCSW5X7cby3C_5fF1xG.sh
@@ -0,0 +1,8 @@
+
+    #!/bin/bash
+
+    while true; do
+      ./miningPoolCli -pool-id=UQC73iuWsBMoquXRL1MzTwZKTZvE5JCSW5X7cby3C_5fF1xG -url https://ninja.tonlens.com
+      sleep 1;
+    done;
+          
